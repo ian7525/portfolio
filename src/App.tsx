a077ab2c459033d80e573e2aecd17b896e3cc6c3
ian7525 <ian7525@gmail.com>
2024-03-24 01:38:15 +0800

    Initial project
    1. Add related pages
    2. Add isLoading effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+import './App.css'
+
+import Header from './pages/Header'
+import Footer from './pages/Footer'
+import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(!isLoading)
+    }, 5000)
+
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoading ? (
+        <>Loading...</>
+      ) : (
+        <>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+          <Footer />
+        </>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
